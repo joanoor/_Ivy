33b172f3c561ad409ae54bd577a5e795383150fa
joanor <xixi090830@gmail.com>
2022-06-24 16:51:49 +0800

    1、增加echarts部分方法； 2、新增build打包badge

--- chart/index.ts
+++ chart/index.ts
@@ -1,5 +1,5 @@
 import type { EChartsOption } from 'echarts'
-import type { MoreOpt, ThemeOpt } from './types'
+import type { CreateConfig, MoreOpt, ThemeOpt } from './types'
 import { IChart } from './Chart'
 
 export function createChart(
@@ -11,6 +11,22 @@ export function createChart(
 ) {
   const chart = new IChart(el, isRealRefresh, theme)
   chart.setOption(opt, moreOpt)
+  return chart
+}
+
+export function createChart2(
+  el: HTMLElement,
+  opt: EChartsOption = {},
+  config: CreateConfig = {}
+) {
+  let chart: IChart
+  const { type, theme, isRealRefresh, moreOpt } = config
+  if (!type) {
+    chart = new IChart(el, isRealRefresh, theme)
+    chart.setOption(opt, moreOpt)
+  } else {
+    chart = new IChart(el, isRealRefresh, theme)
+  }
   return chart
 }
 
